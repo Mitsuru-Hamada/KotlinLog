1aa96de49f8e8934f3f3d73c35e01f07e549488b
mitsuru.hamada <m.hamada@scsk.jp>
2019-07-02 11:12:19 +0900

    修正分。HumanとHuman2にsayとthinkメソッドを追加。 Humanクラスの2つ引数のコンストラクタを削除。

--- app/src/main/java/hamada/mitsuru/techacademy/kotlinlog/Human.kt
+++ app/src/main/java/hamada/mitsuru/techacademy/kotlinlog/Human.kt
@@ -7,10 +7,6 @@ class Human : Animal,Thinkable{
     private var  hobby: String? = null
 
     // 引数付きコンストラクタ
-    constructor(name: String, age: Int): super(name,age){
-        this.name = name
-        this.age = age
-    }
     constructor(name: String, age: Int, hobby: String): super(name,age){
         this.hobby = hobby
     }
